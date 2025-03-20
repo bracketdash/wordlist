bff5083c01e57bd5f2cf0c64f088da0daec1e519
Michael Hatch <michaelhatch@me.com>
2025-03-20 08:24:20 -0700

    add bananagrams wordlist

--- compressBananagrams.js
+++ compressBananagrams.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { compress, decompress } = require("./functions.js");
 
-const wordlist = fs.readFileSync("wordsWordle.txt", "utf-8");
+const wordlist = fs.readFileSync("wordsBananagrams.txt", "utf-8");
 
 const compressed = compress(wordlist);
 
@@ -9,10 +9,10 @@ const decompressed = decompress(compressed);
 
 if (decompressed === wordlist) {
   const finalContents = `const compressedTrie = "${compressed}";`;
-  fs.writeFileSync("wordle.js", finalContents);
+  fs.writeFileSync("bananagrams.js", finalContents);
   const savings = wordlist.length - finalContents.length;
   const kSaved = Math.floor(savings / 1024);
-  console.log(`Success! Produced wordle.js with a ${kSaved}k savings.`);
+  console.log(`Success! Produced bananagrams.js with a ${kSaved}k savings.`);
 } else {
   console.log("Failed.");
 }
